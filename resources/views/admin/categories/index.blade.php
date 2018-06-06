88580e29d57dda08ce5618efea471fc457b11777
L-Ghost <luis.arantes.sp@gmail.com>
2018-06-06 13:07:56 -0300

    categories

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+    <h1>Categories</h1>
+
+    <div class="col-sm6">
+
+        {!! Form::open(['method' => 'POST', 'action' => 'AdminCategoriesController@store']) !!}
+
+            <div class="form-group">
+                {!! Form::label('name', 'Name') !!}
+                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+
+                {!! Form::submit('Create Category', ['class' => 'btn btn-primary']) !!}
+
+            </div>
+
+
+        {!! Form::close() !!}
+
+    </div>
+
+
+    <div class="col-sm6">
+
+        @if('categories')
+
+            <table class="table">
+                <thead>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Created</th>
+                </thead>
+                <tbody>
+
+                    @foreach($categories as $category)
+                        <tr>
+                            <td>{{$category->id}}</td>
+                            <td><a href="{{route('admin.categories.edit', $category->id)}}">{{$category->name}}</a></td>
+                            <td>{{$category->created_at ? $category->created_at->diffForHumans() : 'no date'}}</td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+
+            </table>
+
+        @endif
+
+    </div>
+
+@stop
